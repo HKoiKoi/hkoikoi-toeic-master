2f70e0c46b09f67c36237ca76ab4adc25bd5963c
hkoikoi <sangja234@gmail.com>
2026-08-29 01:06:07 +0900

    ✨ feat(member): Member 사용자 생성 로직 작성 #3

--- backend/src/main/java/com/hkoikoi/toeicMaster/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/hkoikoi/toeicMaster/domain/member/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package com.hkoikoi.toeicMaster.domain.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hkoikoi.toeicMaster.domain.member.entity.Member;
+import com.hkoikoi.toeicMaster.domain.member.enums.OAuth2Provider;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByProviderAndProviderId(OAuth2Provider provider, String providerId);
+
+	boolean existsByNickname(String nickname);
 }
--- backend/src/main/java/com/hkoikoi/toeicMaster/domain/member/service/MemberService.java
+++ backend/src/main/java/com/hkoikoi/toeicMaster/domain/member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.hkoikoi.toeicMaster.domain.member.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.hkoikoi.toeicMaster.domain.member.entity.Member;
+import com.hkoikoi.toeicMaster.domain.member.enums.OAuth2Provider;
 import com.hkoikoi.toeicMaster.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,11 +17,36 @@ public class MemberService {
 
 	// TODO: 내 정보 조회 로직 작성
 
-	// TODO: 사용자 생성 로직 작성
+	@Transactional
+	public Member getOrCreateMember(String email, String nickname, OAuth2Provider provider, String providerId) {
+		return memberRepository.findByProviderAndProviderId(provider, providerId)
+			.orElseGet(() -> {
+				String uniqueNickname = generateUniqueNickname(nickname);
+				Member newMember = Member.create(email, uniqueNickname, provider, providerId);
+				return memberRepository.save(newMember);
+			});
+	}
 
 	// TODO: 내 닉네임 변경 로직 작성
 
 	// TODO: [ADMIN] 사용자 닉네임 변경 로직 작성
 
 	// TODO: [ADMIN] 사용자 권한 변경 로직 작성
+
+	private String generateUniqueNickname(String baseNickname) {
+
+		String nickname = (baseNickname != null && !baseNickname.isBlank()) ? baseNickname : "Member";
+
+		if (nickname.length() > 6) {
+			nickname = nickname.substring(0, 6);
+		}
+
+		String uniqueNickname = nickname;
+		while (memberRepository.existsByNickname(uniqueNickname)) {
+			int randomNum = (int)(Math.random() * 9000) + 1000;
+			uniqueNickname = nickname + randomNum;
+		}
+
+		return uniqueNickname;
+	}
 }
