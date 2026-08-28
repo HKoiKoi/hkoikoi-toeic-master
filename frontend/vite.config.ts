ff02ff045548f767f2041a69916bd683c42d8683
hkoikoi <sangja234@gmail.com>
2026-08-28 17:39:15 +0900

    🚀 chore: Frontend 프로젝트 기본 세팅

---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  envDir: "../",
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
+});
