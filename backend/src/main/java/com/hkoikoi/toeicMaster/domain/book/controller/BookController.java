302c6610321cd0d9906b9a9aa15d864d9653ec9a
hkoikoi <koikoilove99@gmail.com>
2026-09-09 16:23:54 +0900

    🐛 fix(book): Book 목록 조회, 수정 로직 및 API 제대로 작성 #23

--- backend/src/main/java/com/hkoikoi/toeicMaster/domain/book/controller/BookController.java
+++ backend/src/main/java/com/hkoikoi/toeicMaster/domain/book/controller/BookController.java
@@ -1,5 +1,9 @@
 package com.hkoikoi.toeicMaster.domain.book.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hkoikoi.toeicMaster.domain.book.dto.BookCreateRequest;
 import com.hkoikoi.toeicMaster.domain.book.dto.BookCreateResponse;
+import com.hkoikoi.toeicMaster.domain.book.dto.BookPageResponse;
+import com.hkoikoi.toeicMaster.domain.book.dto.BookSearchCondition;
+import com.hkoikoi.toeicMaster.domain.book.dto.BookUpdateRequest;
 import com.hkoikoi.toeicMaster.domain.book.service.BookService;
 import com.hkoikoi.toeicMaster.global.response.ApiResponse;
 
@@ -20,9 +27,27 @@ public class BookController {
 
 	private final BookService bookService;
 
+	@GetMapping
+	public ApiResponse<BookPageResponse> searchBooks(
+		@ModelAttribute BookSearchCondition condition
+	) {
+		return ApiResponse.success(bookService.searchBooks(condition));
+	}
+
 	@PostMapping
 	public ApiResponse<BookCreateResponse> createBook(@Valid @RequestBody BookCreateRequest request) {
 
 		return ApiResponse.success(bookService.createBook(request));
+	}
+
+	@PatchMapping("/{bookId}")
+	public ApiResponse<Void> updateBook(
+		@PathVariable Long bookId,
+		@Valid @RequestBody BookUpdateRequest request
+	) {
+
+		bookService.updateBook(bookId, request);
+
+		return ApiResponse.success();
 	}
 }
